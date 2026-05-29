d558683b07220c49609d429e52f3999f8569efbd
vicsnet <adesanmiadebisi@gmail.com>
2026-05-29 19:55:34 +0100

    test: insurancePool/testProcessClaim added

--- contract/test/InsurancePool.t.sol
+++ contract/test/InsurancePool.t.sol
@@ -60,22 +60,13 @@ contract InsurancePoolTest  is Test {
 
         vm.stopPrank();
     }
-    // Counter public counter;
 
 
-
-    // function setUp() public {
-    //     counter = new Counter();
-    //     counter.setNumber(0);
-    // }
-
-    // function test_Increment() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
-
-    // function testFuzz_SetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
+function testProcessClaim() public{
+    testInsureAsset();
+    uint256 price= 200 * 10 ** 6;
+    insurancePool.processClaim(1, price);
+     insurancePool.processClaim(1, price);
+}
     
 }
